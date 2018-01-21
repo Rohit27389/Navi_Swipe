016c33ea3c2d6899fae46eade49376f69a421b3c
Rohit27389 <rohitkumar.cs14@gmail.com>
2018-01-21 13:22:31 +0530

    first commit

---
+++ app/src/main/java/com/rohit/navigationtab/GamesFragment.java
@@ -0,0 +1,23 @@
+package com.rohit.navigationtab;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Rohit Kumar on 17-01-2018.
+ */
+
+public class GamesFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_games, container, false);
+
+        return rootView;
+    }
+}
